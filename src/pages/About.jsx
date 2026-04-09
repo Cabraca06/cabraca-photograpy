3620d642e1061dd49d5718a2c9c5f4323b647971
Richard cabraca <richardcabraca012@gmail.com>
2026-04-08 20:35:55 -0600

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
   return (
     <div className='about-container'>
       
-      <p> <h1>About Me</h1>Hi, I'm a passionate photographer with a love for capturing moments and telling stories through my lens. With years of experience in various photography styles, I strive to create compelling and visually stunning images that resonate with viewers. Whether it's portrait, landscape, or street photography, I am dedicated to honing my craft and sharing my unique perspective with the world.</p>
+      <p> <h1>About Me</h1>Hi, I'm a passionate photographer with a love for capturing moments and telling stories through my lens. With years of experience in various photography styles, I strive to create compelling and visually stunning images that resonate with viewers. Whether it's portrait, landscape, or street photography, I am dedicated to honing my craft and sharing my unique perspective with the world...</p>
       <img src={imgbout} alt="" className='about-image' />
     </div>
    
