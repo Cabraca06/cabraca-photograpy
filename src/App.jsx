b89a9cc0792138900ff514ea40c3087c62f4e784
Richard cabraca <richardcabraca012@gmail.com>
2026-04-12 11:09:13 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import About from './pages/About'
 import Projects from './pages/Projects'
@@ -14,15 +14,15 @@ function App() {
   return (
       <> 
     <Navbar />
-    <Router> 
+    <BrowserRouter basename='/cabraca-photography'> 
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="projects" element={<Projects />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   
  </>
 );
