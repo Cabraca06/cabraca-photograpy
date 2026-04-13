4efad47cfd55bd49f45df49b633f056128f1f449
Richard cabraca <richardcabraca012@gmail.com>
2026-04-12 19:45:16 -0600

    Update Home.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import '../styles/About.css'
-import imgbout from '../assets/img/_MG_0333.jpg';
+import imgAbout from '../assets/img/_MG_0333.jpg';
 
 function About() {
-
   return (
-    <div className='about-container'>
-      
-      <p> <h1>About Me</h1>Hi, I'm a passionate photographer with a love for capturing moments and telling stories through my lens. With years of experience in various photography styles, I strive to create compelling and visually stunning images that resonate with viewers. Whether it's portrait, landscape, or street photography, I am dedicated to honing my craft and sharing my unique perspective with the world...</p>
-      <img src={imgbout} alt="" className='about-image' />
+    <div className="about-container">
+
+      <div className="about-image-wrapper">
+        <img src={imgAbout} alt="Cabraca Photography" className="about-image" />
+      </div>
+
+      <div className="about-text">
+        <h1>About Me</h1>
+        <p>
+          Hi, I'm a passionate photographer with a love for capturing moments
+          and telling stories through my lens. With years of experience in
+          various photography styles, I strive to create compelling and visually
+          stunning images that resonate with viewers.
+        </p>
+        <p>
+          Whether it's portrait, landscape, or street photography, I am
+          dedicated to honing my craft and sharing my unique perspective
+          with the world.
+        </p>
+      </div>
+
     </div>
-   
-   
   )
 }
 
